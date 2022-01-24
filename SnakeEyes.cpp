9a7c05912f76a38591b111c5cb3f06b22f7fbff1
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-01-24 16:00:19 -0500

    Create SnakeEyes.cpp

---
+++ SnakeEyes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(unsigned(time(0))); //Randomize the random #s
+    
+    //Start your loop here
+    
+    //Roll 2 dice
+    int d1 = rand() % 6 + 1;
+    int d2 = rand() % 6 + 1;
+    //Display roll
+    cout << "You rolled: " << d1 << " " << d2 << endl;
+
+}
